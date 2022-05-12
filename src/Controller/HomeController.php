112219271f8235dd828a3ebc6199bd8027c23ce9
baloolc <viann45@hotmail.fr>
2022-05-12 22:15:50 +0200

    sécurisation du formulaire

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,4 +11,24 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/index.html.twig');
     }
+
+    public function validate()
+    {
+        $contact = [];
+        $errors = [];
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $contact = array_map("trim", $_POST);
+
+            if (empty($contact["url"])) {
+                $errors[] = "L'url est obligatoire";
+            }
+            if (!filter_var($contact['url'], FILTER_VALIDATE_URL)) {
+                $errors[] = "Mauvais format pour l'url ";
+            }
+            if (empty($errors)) {
+                header('Location: /index.html.twig');
+            }
+        }
+        return $this->twig->render('Home/index.html.twig', ['errors' => $errors, 'contact' => $contact]);
+    }
 }
