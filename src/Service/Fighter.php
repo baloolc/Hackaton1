a1d08f3678b4917a5cf8c568a55c970bba27e6db
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-05-12 14:55:24 +0200

    Class for fighter, hero and enemy

--- src/Service/Fighter.php
+++ src/Service/Fighter.php
@@ -2,4 +2,53 @@
 
 namespace App\Service;
 
-abstract 
+abstract class Fighter
+{
+    private string $name;
+    protected int $x;
+    protected int $y;
+    protected string $image = '';
+
+    public function __construct(string $name, int $x, int $y)
+    {
+        $this->name = $name;
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    public function setX($x): self
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    public function setY($y): self
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+}
